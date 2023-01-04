4d5bf2680dcef1b6e18fe5dd4d84984ed98382b6
VMurtazin <obdorsk@gmail.com>
2023-01-04 18:05:36 +0500

    add_main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,30 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args)  {
+        Util util=new Util();
+        String query="select * from users";
+        try {
+            Statement statement=util.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                User user=new User();
+                user.setId((long) resultSet.getInt(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge((byte) resultSet.getInt(4));
+
+                System.out.println(user);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+//private static final String DB_DRIVER="com.mysql.cj.jdbc.Driver";
+private static final String DB_URL="jdbc:mysql://localhost:3306/mydb1";
+private static final String DB_USERNAME="root";
+private static final String DB_PASSWORD="rootroot";
+
+private Connection connection;
+
+public Util(){
+    try {
+        connection= DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+        System.out.println("connection ok");
+    }catch (SQLException e){
+        System.out.println("connection Nok");
+        e.printStackTrace();
+    }
+}
+
+public Connection getConnection(){
+    return connection;
+}
+
 }
